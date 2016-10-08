a94a6a0d553b3aa0d34785e465e6643ea39cf5ec
Ivan Mironchik <me.roaiven@gmail.com>
2016-10-08 22:05:55 +0300

    Refactor createShelfReducer function

--- src/helpers/createShelfReducer.ts
+++ src/helpers/createShelfReducer.ts
@@ -1,47 +1,40 @@
-// at first create tests with this implementation and then refactor it with using createReducer
 import assign from '../utils/assign';
+import createReducer from './createReducer';
 
 import { IAsyncActionTypes, IActionHandlersMap, IAnyObject } from '../interfaces';
 
-function createShelfReducer([request, success, failure]: IAsyncActionTypes, customState?: IAnyObject, customMap?: IActionHandlersMap) {
-  return (
-    state = assign(customState, {
+function createShelfReducer(
+  [request, success, failure]: IAsyncActionTypes,
+  customState?: IAnyObject,
+  customMap?: IActionHandlersMap
+) {
+  return createReducer(
+    assign(customState, {
       isFetching: false,
       lastUpdated: undefined,
       error: undefined,
     }),
-    action = {}
-  ) => {
-    const { payload } = action;
+    assign(customMap, {
+      [request]: (state: IState, payload: IActionPayload) => assign(state, {
+        isFetching: true,
+      }),
+      [success]: (state: IState, payload: IActionPayload) => assign(state, {
+        isFetching: false,
+        lastUpdated: payload.receivedAt,
+        error: undefined,
+      }),
+      [failure]: (state: IState, payload: IActionPayload) => assign(state, {
+        isFetching: false,
+        error: payload.message,
+      }),
+    })
+  );
+}
 
-    if (customMap) {
-      for (let actionType in customMap) {
-        if (actionType === actionType) {
-          return customMap[actionType](state, payload);
-        }
-      }
-    }
-
-    switch (action.type) {
-      case request:
-        return assign(state, {
-          isFetching: true,
-        });
-      case success:
-        return assign(state, {
-          isFetching: false,
-          lastUpdated: payload.receivedAt,
-          error: undefined,
-        });
-      case failure:
-        return assign(state, {
-          isFetching: false,
-          error: payload.message,
-        });
-      default:
-        return state;
-    }
-  }
+interface IState {
+  isFetching: boolean,
+  lastUpdated?: number,
+  error?: string,
 }
 
 export default createShelfReducer;
