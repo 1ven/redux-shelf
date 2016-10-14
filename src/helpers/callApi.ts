59ea43002e8e3caee744da3b77a2a99f62c20e46
Ivan Mironchik <me.roaiven@gmail.com>
2016-10-14 23:59:26 +0300

    Remove url condition

--- src/helpers/callApi.ts
+++ src/helpers/callApi.ts
@@ -3,9 +3,7 @@ import * as axios from 'axios';
 /* import { ICallApi } from '../interfaces'; */
 
 const callApi = function(url, method, data) {
-  const _url = typeof url === 'function' ? url() : url;
-
-  return axios({ url: _url, method, data })
+  return axios({ url, method, data })
     .then(({ data }) => ({
       result: data,
       receivedAt: Date.now(),
