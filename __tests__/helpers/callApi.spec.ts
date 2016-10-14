3187bf382db8836f67d138b34e5847a80df238c3
Ivan Mironchik <me.roaiven@gmail.com>
2016-10-15 00:12:55 +0300

    Add callApi test file

---
+++ __tests__/helpers/callApi.spec.ts
@@ -0,0 +1,7 @@
+import callApi from '../../src/helpers/callApi';
+
+describe('callApi', () => {
+  it('should call to remote server by given params and assign response to `result` prop', () => {
+    
+  });
+});
