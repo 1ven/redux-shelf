24676e7251816d15c01c5d433cab07e15ca4068d
Ivan Mironchik <me.roaiven@gmail.com>
2016-10-15 17:54:03 +0300

    Implement resolveUrl util

---
+++ __tests__/utils/resolveUrl.spec.ts
@@ -0,0 +1,23 @@
+import resolveUrl from '../../src/utils/resolveUrl';
+
+describe('resolveUrl', () => {
+  const assertionsData = [{
+    args: ['http://site.com', 'test/test2'],
+    expectingValue: 'http://site.com/test/test2',
+  }, {
+    args: ['http://site.com/', 'test/test2'],
+    expectingValue: 'http://site.com/test/test2',
+  }, {
+    args: ['http://site.com', '/test/test2'],
+    expectingValue: 'http://site.com/test/test2',
+  }, {
+    args: ['http://site.com/', '/test/test2'],
+    expectingValue: 'http://site.com/test/test2',
+  }];
+
+  assertionsData.forEach(item => {
+    it(`should resolve url by given "${item.args}" params`, () => {
+      expect(resolveUrl.apply(null, item.args)).toBe(item.expectingValue);
+    });
+  });
+});
---
+++ src/utils/resolveUrl.ts
@@ -0,0 +1,8 @@
+const resolveUrl = function(root, pathname) {
+  const rootWihoutTrailingSlash = root.slice(-1) !== '/' ? root : root.slice(0, -1);
+  const pathnameWithoutLeadingSlash = pathname[0] !== '/' ? pathname : pathname.slice(1);
+
+  return rootWihoutTrailingSlash + '/' + pathnameWithoutLeadingSlash;
+}
+
+export default resolveUrl;
